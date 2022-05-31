7a5597fd0688331f37b034472481ea5b275f6d3d
Labonno <macbook@MacBooks-MacBook-Air.local>
2022-05-31 12:14:21 +0600

    style:slidder added

--- src/components/layouts/homepage-layout-5.vue
+++ src/components/layouts/homepage-layout-5.vue
@@ -5,19 +5,28 @@
           <button class="rounded-full  h-2   text-center border-b"  :class="{'bg-purpleText  w-10':tab==index, 'bg-gray-300 w-5':tab!=index}"  @click="changeTabdata(index)"></button>
         </div>
       </div>
-      
-      <div class="grid grid-cols-2 gap-20">
+      <div class=" relative overflow-hidden w-full">
+        <div v-for="i,index in totalSlide" :id="`slide-${slideNumber}`" :key="index" :class="`${active ===index ? 'active':'left-full'}`" class="carousel-item inset-0 relative w-full transform transition-all delay-700 duration-500 ease-in-out">
+        <div class="grid grid-cols-2 gap-20">
           <TabCard2 />
           <TabCard2 />
+        </div>
+
       </div>
+      </div>
+      <!-- <div  class="grid grid-cols-2 gap-20">
+          <TabCard2 />
+          <TabCard2 />
+      </div> -->
       <div class="grid place-content-center pt-10">
           <div class="flex gap-10">
-              <div class="rounded-full w-14 h-14 circleShadow2 grid place-content-start">
-                  <svg xmlns="http://www.w3.org/2000/svg" height="56" fill="#644F9C" width="56"><path d="M28.05 36 16 23.95 28.05 11.9 30.2 14.05 20.3 23.95 30.2 33.85Z"/></svg>
-              </div>
-              <div class="rounded-full w-14 h-14 circleShadow2 grid place-content-end">
-                  <svg class="pt-3" xmlns="http://www.w3.org/2000/svg" height="56" width="56" fill="#644F9C"><path d="M18.75 36 16.6 33.85 26.5 23.95 16.6 14.05 18.75 11.9 30.8 23.95Z"/></svg>
-              </div>
+              <button :disable="disableDec" class="rounded-full w-14 h-14 circleShadow2 grid place-content-end" @click="Dec()">
+                   <svg xmlns="http://www.w3.org/2000/svg" height="56"  :fill="disableDec ? '#A8A29E' : '#644F9C'"  width="56"><path d="M28.05 36 16 23.95 28.05 11.9 30.2 14.05 20.3 23.95 30.2 33.85Z"/></svg>
+                  
+              </button>
+              <button :disabled="disableInc" class="rounded-full w-14 h-14 circleShadow2 grid place-content-start" @click="Inc()">
+                 <svg class="pt-3" xmlns="http://www.w3.org/2000/svg" height="56" width="56" :fill="disableInc ? '#A8A29E' : '#644F9C'" ><path d="M18.75 36 16.6 33.85 26.5 23.95 16.6 14.05 18.75 11.9 30.8 23.95Z"/></svg>
+              </button>
           </div>
       </div>
   </div>
@@ -30,6 +39,11 @@ export default {
      return {
          tabs: ['POP-UNDER', 'BANNER AD','NATIVE', 'SKIM'],
          tab: 0,
+         active : 0,
+         totalSlide: 4,
+         slideNumber: 1,
+         disableInc:false,
+         disableDec:true,
          cardData1: {images:[],
          title: 'POP-UNDER',
          description: 'This is one of the most popular ads. After user/’s click, this ad opens landing page in behind. So, the user experience is not hampered much.'},
@@ -50,6 +64,33 @@ export default {
  methods:{
    changeTabdata(index) {
      this.tab = index
+   },
+   Inc () 
+   {
+     this.disableDec = false
+     if(this.slideNumber > this.totalSlide  )
+     {
+       this.slideNumber = 1
+       this.disableInc = true
+     }
+     this.active = this.slideNumber
+     this.slideNumber ++
+     console.log(this.slideNumber)
+   },
+   Dec () {
+     this.disableInc = false
+     if(this.totalSlide > 1)
+     {
+       if(this.slideNumber > this.totalSlide  ){
+       this.slideNumber = 1
+     }
+     this.active = this.slideNumber
+     this.slideNumber --
+     }
+     else{
+       this.disableDec = true
+     }
+     console.log(this.slideNumber)
    }
  }
 }
@@ -58,5 +99,11 @@ export default {
 <style>
 .circleShadow2 {
 box-shadow: 2px 2px 5px #a3a1c2;
+}
+.left-full {
+  left: -100%;
+}
+.carousel-item.active {
+  left: 0;
 }
 </style>
